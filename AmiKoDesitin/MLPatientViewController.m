6095a9c6ffdb2efcdf552bfe8b1230f8c416691a
Alessandro Bettarini <abettarini@icloud.com>
2018-03-09 11:22:45 +0100

    Enable Save in Patient Edit for non mandatory fields

--- AmiKoDesitin/MLPatientViewController.m
+++ AmiKoDesitin/MLPatientViewController.m
@@ -410,10 +410,20 @@ enum {
 #ifdef DEBUG
     //NSLog(@"%s tag:%ld", __FUNCTION__, textField.tag);
 #endif
+    if ((textField.tag == 6) ||     // country
+        (textField.tag == 9) ||     // weight
+        (textField.tag == 10) ||    // height
+        (textField.tag == 11) ||    // phone
+        (textField.tag == 12))      // email
+    {
+        return TRUE;  // Allow non mandatory fields to be empty
+    }
+        
     UIColor *lightRed = [UIColor colorWithRed:1.0
                                         green:0.0
                                          blue:0.0
                                         alpha:0.3];
+
     BOOL valid = TRUE;
     if ([textField.text isEqualToString:@""])
         valid = FALSE;
